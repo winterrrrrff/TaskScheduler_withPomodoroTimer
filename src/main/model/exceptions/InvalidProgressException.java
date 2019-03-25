b0849745d57d8283dd8e1882f5afec12eaca9234
m8n2b <m8n2b@ugrad.cs.ubc.ca>
2019-03-25 13:11:36 -0700

    Seventh Attempt for Phase 5

--- src/main/model/exceptions/InvalidProgressException.java
+++ src/main/model/exceptions/InvalidProgressException.java
@@ -1,4 +1,12 @@
 package model.exceptions;
 
 public class InvalidProgressException extends IllegalArgumentException {
+
+    public InvalidProgressException() {
+        super();
+    }
+
+    public InvalidProgressException(String errorMsg) {
+        super(errorMsg);
+    }
 }
--- src/main/model/exceptions/NegativeInputException.java
+++ src/main/model/exceptions/NegativeInputException.java
@@ -1,4 +1,12 @@
 package model.exceptions;
 
 public class NegativeInputException extends IllegalArgumentException {
+
+    public NegativeInputException() {
+        super();
+    }
+
+    public NegativeInputException(String errorMsg) {
+        super(errorMsg);
+    }
 }
